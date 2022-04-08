06434c284a9a20300c613e8f1d335f9a060b14d6
Arthur LAINE <arlaine@student.42.fr>
2022-04-08 16:09:28 +0200

    finished fit_ function that aims to implement a simple gradient descent

---
+++ ex02/fit.py
@@ -0,0 +1,21 @@
+import numpy as np
+from vec_gradient import add_intercept, gradient
+
+def fit_(x, y, theta, alpha, max_iter):
+    if not isinstance(x, np.ndarray) or not isinstance(y, np.ndarray) or not isinstance(theta, np.ndarray) or\
+            not isinstance(alpha, float) or not isinstance(max_iter, int):
+                return None
+    if x.shape[1] != 1 or y.shape[1] != 1 or theta.shape != (2, 1):
+        return None
+    if max_iter <= 0:
+        return None
+    for i in range(max_iter):
+        # Compute gradients
+        gradients = gradient(x, y, theta)
+        # Update theta in regard to new gradients and learning_rate
+        theta = theta - (alpha * gradients)
+        # Loop this process
+    return theta
+
+def predict(x, theta):
+    return np.sum((theta[0], theta[1] * x))
---
+++ ex02/vec_gradient.py
@@ -0,0 +1,31 @@
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+
+
+def add_intercept(x):
+    """
+    Adds a column of 1's to the non-empty numpy.array x
+    """
+    if not isinstance(x, np.ndarray):
+        return None
+    if len(x.shape) != 2 or x.shape[1] != 1:
+        return None
+    try:
+        new_array = np.array(x, dtype=float)
+    except ValueError:
+        return None
+    intercept_ = np.ones((1, len(x)), dtype=float)
+    return np.insert(new_array, 0, intercept_, axis=1)
+
+def gradient(x, y, theta):
+    if not isinstance(x, np.ndarray) or not isinstance(y, np.ndarray) or not isinstance(theta, np.ndarray):
+        return None
+    if len(x) == 0 or len(y) == 0 or len(theta) == 0:
+        return None
+    if x.shape[1] != 1 or y.shape[1] != 1 or theta.shape != (2, 1):
+        return None
+    x = add_intercept(x)
+    y_hat = np.sum((theta[0], theta[1] * y))
+    gradients = (1 / len(x)) * np.dot(x.T, np.dot(x, theta) - y)
+    return gradients
