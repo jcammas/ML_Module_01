77db830c740f497a88b34461d9e9d76239cba9e4
Arthur LAINE <arlaine@student.42.fr>
2022-04-08 15:45:26 +0200

    finished simple_gradients computation using for loop

---
+++ ex00/gradient.py
@@ -0,0 +1,21 @@
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+
+def simple_gradient(x, y, theta):
+    if not isinstance(x, np.ndarray) or not isinstance(y, np.ndarray) or not isinstance(theta, np.ndarray):
+        return None
+    if len(x) == 0 or len(y) == 0 or len(theta) == 0:
+        return None
+    if x.shape[1] != 1 or y.shape[1] != 1 or theta.shape != (2, 1):
+        return None
+    gradients = np.zeros((2, 1))
+    y_hat = np.sum((theta[0], theta[1] * x))
+    for i in range(len(x)):
+        try:
+            gradients[0] += y_hat[i] - y[i]
+            gradients[1] += (y_hat[i] - y[i]) * x[i]
+        except (np.core._exceptions.UFuncTypeError, TypeError):
+            return None
+    gradients = gradients * (1 / len(x))
+    return gradients
